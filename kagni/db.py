5aa5248e58acc3aae3fbcaf3791d4b35215549f9
özgür altunyurt <altunyurt@gmail.com>
2019-04-12 14:35:05 +0300

    added db interface

---
+++ kagni/db.py
@@ -0,0 +1,31 @@
+import apsw
+import pickle
+
+
+class DB:
+    def __init__(self, path):
+        self.con = apsw.Connection(path)
+        cur = self.con.cursor()
+
+        cur.execute("""create table if not exists data (key text not null, value blob not null);""")
+
+        cur.close()
+
+    def dump(self, data):
+        ''' needs to be run as a task '''
+        cur = self.con.cursor()
+        for key, val in data.items():
+            cur.execute("""insert into data values(?,?)""", [key, pickle.dumps(val)])
+
+        cur.close()
+
+    def load(self):
+        cur = self.con.cursor()
+        cur.execute("""select key, value from data""")
+        for (key, val) in cur.fetchall():
+            print(key, pickle.loads(val))
+
+        cur.close()
+
+
+db = DB(":memory:")
