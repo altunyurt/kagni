5a85d6b77c9d0ade4701dcb889bff2fe34369f17
özgür altunyurt <altunyurt@gmail.com>
2019-05-01 18:26:12 +0300

    added SDIFF SDIFFSTORE SINTER SINTERSTORE SISMEMBER SMOVE SPOP SRANDMEMBER and tests

--- kagni/commands/set.py
+++ kagni/commands/set.py
@@ -1,3 +1,5 @@
+from random import randrange, sample
+from functools import reduce
 from typing import List
 import fnmatch
 import re
@@ -13,10 +15,10 @@ class CommandSetMixin:
     def SADD(self, key: bytes, *vals: List[bytes]) -> int:
         if key not in self.data:
             self.data[key] = set()
-        
+
         initial = len(self.data[key])
         self.data[key].update(set(vals))
-        return len(self.data[key]) - initial 
+        return len(self.data[key]) - initial
 
     @command_decorator(b"SCARD")
     def SCARD(self, key: bytes) -> int:
@@ -35,41 +37,95 @@ class CommandSetMixin:
     def SREM(self, key: bytes, *val: List[bytes]) -> int:
         if key not in self.data:
             return 0
+
         initial = len(self.data[key])
         self.data[key] = self.data[key].difference(set(val))
-        return initial - len(self.data[key]) 
+        return initial - len(self.data[key])
 
     @command_decorator(b"SDIFF")
-    def SDIFF(self, key: bytes, val: bytes):
-        pass
+    def SDIFF(self, *keys: List[bytes]) -> list:
+        first = self.data.get(keys[0], set())
+        rest = (self.data.get(key, set()) for key in keys[1:])
+        retval = reduce(lambda _s1, _s2: _s1 - _s2, rest, first)
+        return list(retval)
 
     @command_decorator(b"SDIFFSTORE")
-    def SDIFFSTORE(self, key: bytes, val: bytes):
-        pass
+    def SDIFFSTORE(self, target: bytes, *keys: List[bytes]) -> int:
+        first = self.data.get(keys[0], set())
+        rest = (self.data.get(key, set()) for key in keys[1:])
+        self.data[target] = reduce(lambda _s1, _s2: _s1 - _s2, rest, first)
+        return len(self.data[target])
 
-    @command_decorator(b"INTER")
-    def INTER(self, key: bytes, val: bytes):
-        pass
+    @command_decorator(b"SINTER")
+    def SINTER(self, *keys: List[bytes]) -> list:
+        first = self.data.get(keys[0], set())
+        rest = (self.data.get(key, set()) for key in keys[1:])
+        retval = reduce(lambda _s1, _s2: _s1 & _s2, rest, first)
+        return list(retval)
 
     @command_decorator(b"SINTERSTORE")
-    def SINTERSTORE(self, key: bytes, val: bytes):
-        pass
+    def SINTERSTORE(self, target: bytes, *keys: List[bytes]) -> int:
+        first = self.data.get(keys[0], set())
+        rest = (self.data.get(key, set()) for key in keys[1:])
+        self.data[target] = reduce(lambda _s1, _s2: _s1 & _s2, rest, first)
+        return len(self.data[target])
 
     @command_decorator(b"SISMEMBER")
-    def SISMEMBER(self, key: bytes, val: bytes):
-        pass
+    def SISMEMBER(self, key: bytes, val: bytes) -> int:
+        if key not in self.data:
+            return 0
+        return 1 if val in self.data[key] else 0
 
     @command_decorator(b"SMOVE")
-    def SMOVE(self, key: bytes, val: bytes):
-        pass
+    def SMOVE(self, source: bytes, target: bytes, val: bytes) -> int:
+        if source not in self.data:
+            return 0
+
+        if val not in self.data[source]:
+            return 0
+
+        self.data[source].discard(val)
+        # this might be different in redis
+        if target not in self.data:
+            self.data[target] = set()
+
+        self.data[target].add(val)
+        return 1
 
     @command_decorator(b"SPOP")
-    def SPOP(self, key: bytes, val: bytes):
-        pass
+    def SPOP(self, key: bytes, count: int = None) -> (bytes, List[bytes]):
+        if key not in self.data:
+            return NIL
+
+        set_len = len(self.data[key])
+        count = count if count else 1
+
+        if 1 in (count, set_len):
+            return self.data[key].pop()
+
+        if count > set_len:
+            return list(self.data[key])
+
+        # vals hashed with random salt, so we could say that the pop would work randomly
+        # https://docs.python.org/3.5/reference/datamodel.html#object.__hash__
+        return [self.data[key].pop() for i in range(count)]
 
     @command_decorator(b"SRANDMEMBER")
-    def SRANDMEMBER(self, key: bytes, val: bytes):
-        pass
+    def SRANDMEMBER(self, key: bytes, count: int = None) -> (bytes, List[bytes]):
+        if key not in self.data:
+            return NIL
+
+        set_len = len(self.data[key])
+        count = count if count else 1
+
+        if count > set_len > 1:
+            return list(self.data[key])
+
+        samples = sample(self.data[key], count)
+        if 1 in (count, set_len):
+            return samples[0]
+
+        return samples
 
     @command_decorator(b"SSCAN")
     def SSCAN(self, key: bytes, val: bytes):
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -640,7 +640,7 @@ def test_commands():
         {
             "name": "Check SREM return value for existing key",
             "command": "SREM",
-            "args": [b"k", b"1", b"2", b"hm",b"3", b"rn", b"78"],
+            "args": [b"k", b"1", b"2", b"hm", b"3", b"rn", b"78"],
             "returns": 3,
             "depends": [
                 {
@@ -650,6 +650,310 @@ def test_commands():
                 }
             ],
         },
+        {
+            "name": "Check SDIFF value for non existing key",
+            "command": "SDIFF",
+            "args": [b"k", b"a", b"b"],
+            "returns": [],
+        },
+        {
+            "name": "Check SDIFF value for existing key with non existing diff keys ",
+            "command": "SDIFF",
+            "args": [b"k", b"a", b"b"],
+            "returns": list(set([b"1", b"2", b"3", b"4"])),
+            "depends": [
+                {"command": "SADD", "args": [b"k", b"1", b"2", b"3", b"4"], "returns": 4}
+            ],
+        },
+        {
+            "name": "Check SDIFF return value for existing key",
+            "command": "SDIFF",
+            "args": [b"k", b"a", b"b"],
+            "returns": list(set([b"1", b"2", b"3"])),
+            "depends": [
+                {
+                    "command": "SADD",
+                    "args": [b"k", b"1", b"2", b"3", b"4", b"5", b"6", b"7", b"8", b"9"],
+                    "returns": 9,
+                },
+                {
+                    "command": "SADD",
+                    "args": [b"a", b"4", b"5", b"6", b"7", b"8", b"9"],
+                    "returns": 6,
+                },
+                {
+                    "command": "SADD",
+                    "args": [b"b", b"10", b"20", b"30", b"40"],
+                    "returns": 4,
+                },
+            ],
+        },
+        {
+            "name": "Check SDIFFSTORE value for non existing key",
+            "command": "SDIFFSTORE",
+            "args": [b"k", b"a", b"b", b"c"],
+            "returns": 0,
+        },
+        {
+            "name": "Check SDIFFSTORE value for existing key with non existing diff keys ",
+            "command": "SDIFFSTORE",
+            "args": [b"k", b"a", b"b", b"c"],
+            "returns": 0,
+            "depends": [
+                {"command": "SADD", "args": [b"k", b"1", b"2", b"3", b"4"], "returns": 4}
+            ],
+        },
+        {
+            "name": "Check SDIFFSTORE value for existing key with some existing diff keys ",
+            "command": "SDIFFSTORE",
+            "args": [b"k", b"a", b"b", b"c"],
+            "returns": 5,
+            "depends": [
+                {"command": "SADD", "args": [b"k", b"1", b"3", b"4"], "returns": 4},
+                {
+                    "command": "SADD",
+                    "args": [b"a", b"10", b"20", b"30", b"40", b"50"],
+                    "returns": 4,
+                },
+            ],
+        },
+        {
+            "name": "Check SDIFFSTORE return value for existing key",
+            "command": "SDIFFSTORE",
+            "args": [b"k", b"a", b"b", b"c"],
+            "returns": 2,
+            "depends": [
+                {
+                    "command": "SADD",
+                    "args": [b"a", b"1", b"2", b"3", b"4", b"5", b"6", b"7", b"8", b"9"],
+                    "returns": 9,
+                },
+                {
+                    "command": "SADD",
+                    "args": [b"b", b"4", b"5", b"6", b"7", b"8", b"9"],
+                    "returns": 6,
+                },
+                {
+                    "command": "SADD",
+                    "args": [b"c", b"2", b"10", b"20", b"30", b"40"],
+                    "returns": 4,
+                },
+            ],
+        },
+        {
+            "name": "Check SINTER value for non existing key",
+            "command": "SINTER",
+            "args": [b"k", b"a", b"b"],
+            "returns": [],
+        },
+        {
+            "name": "Check SINTER value for existing key with non existing inter keys ",
+            "command": "SINTER",
+            "args": [b"k", b"a", b"b"],
+            "returns": [],
+            "depends": [
+                {"command": "SADD", "args": [b"k", b"1", b"2", b"3", b"4"], "returns": 4}
+            ],
+        },
+        {
+            "name": "Check SINTER return value for existing key",
+            "command": "SINTER",
+            "args": [b"k", b"a", b"b"],
+            "returns": list(set([b"5", b"6"])),
+            "depends": [
+                {
+                    "command": "SADD",
+                    "args": [b"k", b"1", b"2", b"3", b"4", b"5", b"6", b"7", b"8", b"9"],
+                    "returns": 9,
+                },
+                {
+                    "command": "SADD",
+                    "args": [b"a", b"4", b"5", b"6", b"7", b"8", b"9"],
+                    "returns": 6,
+                },
+                {
+                    "command": "SADD",
+                    "args": [b"b", b"10", b"20", b"30", b"40", b"5", b"6"],
+                    "returns": 4,
+                },
+            ],
+        },
+        {
+            "name": "Check SINTERSTORE value for non existing key",
+            "command": "SINTERSTORE",
+            "args": [b"k", b"a", b"b", b"c"],
+            "returns": 0,
+        },
+        {
+            "name": "Check SINTERSTORE value for existing key with non existing diff keys ",
+            "command": "SINTERSTORE",
+            "args": [b"k", b"a", b"b", b"c"],
+            "returns": 0,
+            "depends": [
+                {"command": "SADD", "args": [b"k", b"1", b"2", b"3", b"4"], "returns": 4}
+            ],
+        },
+        {
+            "name": "Check SINTERSTORE value for existing key with some existing diff keys ",
+            "command": "SINTERSTORE",
+            "args": [b"k", b"a", b"b"],
+            "returns": 3,
+            "depends": [
+                {
+                    "command": "SADD",
+                    "args": [b"k", b"1", b"3", b"4", b"89"],
+                    "returns": 4,
+                },
+                {
+                    "command": "SADD",
+                    "args": [b"a", b"10", b"20", b"30", b"40", b"50"],
+                    "returns": 4,
+                },
+                {"command": "SADD", "args": [b"b", b"10", b"40", b"50"], "returns": 4},
+            ],
+        },
+        {
+            "name": "Check SISMEMBER with nonexisting key",
+            "command": "SISMEMBER",
+            "args": [b"k", b"a"],
+            "returns": 0,
+        },
+        {
+            "name": "Check SISMEMBER with nonmember value",
+            "command": "SISMEMBER",
+            "args": [b"k", b"a"],
+            "returns": 0,
+            "depends": [
+                {"command": "SADD", "args": [b"k", b"1", b"3", b"4", b"89"], "returns": 4}
+            ],
+        },
+        {
+            "name": "Check SISMEMBER with existing value",
+            "command": "SISMEMBER",
+            "args": [b"k", b"a"],
+            "returns": 1,
+            "depends": [
+                {
+                    "command": "SADD",
+                    "args": [b"k", b"1", b"3", b"a", b"4", b"89"],
+                    "returns": 4,
+                }
+            ],
+        },
+        {
+            "name": "Check SMOVE with nonexisting key",
+            "command": "SMOVE",
+            "args": [b"s", b"t", b"val"],
+            "returns": 0,
+        },
+        {
+            "name": "Check SMOVE with existing key nonmember value",
+            "command": "SMOVE",
+            "args": [b"s", b"t", b"val"],
+            "returns": 0,
+            "depends": [{"command": "SADD", "args": [b"s", b"notval"], "returns": 1}],
+        },
+        {
+            "name": "Check SMOVE with member value non existing target",
+            "command": "SMOVE",
+            "args": [b"s", b"t", b"val"],
+            "returns": 1,
+            "depends": [{"command": "SADD", "args": [b"s", b"val"], "returns": 1}],
+            "expects": lambda cmds: b"val" not in cmds.data[b"s"]
+            and b"val" in cmds.data[b"t"],
+        },
+        {
+            "name": "Check SMOVE with member value  existing target having the same value",
+            "command": "SMOVE",
+            "args": [b"s", b"t", b"val"],
+            "returns": 1,
+            "depends": [
+                {"command": "SADD", "args": [b"s", b"val"], "returns": 1},
+                {"command": "SADD", "args": [b"t", b"val"], "returns": 1},
+            ],
+            "expects": lambda cmds: b"val" not in cmds.data[b"s"]
+            and b"val" in cmds.data[b"t"],
+        },
+        {
+            "name": "Check SMOVE with member value  existing target not having the value",
+            "command": "SMOVE",
+            "args": [b"s", b"t", b"val"],
+            "returns": 1,
+            "depends": [
+                {"command": "SADD", "args": [b"s", b"val"], "returns": 1},
+                {"command": "SADD", "args": [b"t", b"notval"], "returns": 1},
+            ],
+            "expects": lambda cmds: b"val" not in cmds.data[b"s"]
+            and b"val" in cmds.data[b"t"],
+        },
+        {
+            "name": "Check SPOP with nonexisting key",
+            "command": "SPOP",
+            "args": [b"k"],
+            "returns": NIL,
+        },
+        {
+            "name": "Check SPOP with only existing key",
+            "command": "SPOP",
+            "args": [b"k"],
+            "returns": b"1",
+            "depends": [{"command": "SADD", "args": [b"k", b"1"], "returns": 1}],
+        },
+        {
+            "name": "Check SPOP with a sample size less than set size",
+            "command": "SPOP",
+            "args": [b"k", b"3"],
+            "returns": lambda rv, cs: set([b"1", b"2", b"3", b"4", b"5"]).issuperset(set(rv)),
+            "depends": [
+                {
+                    "command": "SADD",
+                    "args": [b"k", b"1", b"2", b"3", b"4", b"5"],
+                    "returns": 1,
+                }
+            ],
+        },
+        {
+            "name": "Check SPOP with count bigger than set size",
+            "command": "SPOP",
+            "args": [b"k", b"3"],
+            "returns": list(set([b"1", b"2"])),
+            "depends": [{"command": "SADD", "args": [b"k", b"1", b"2"], "returns": 2}],
+        },
+
+        {
+            "name": "Check SRANDMEMBER with nonexisting key",
+            "command": "SRANDMEMBER",
+            "args": [b"k"],
+            "returns": NIL,
+        },
+        {
+            "name": "Check SRANDMEMBER with only existing key",
+            "command": "SRANDMEMBER",
+            "args": [b"k"],
+            "returns": b"1",
+            "depends": [{"command": "SADD", "args": [b"k", b"1"], "returns": 1}],
+        },
+        {
+            "name": "Check SRANDMEMBER with a sample size less than set size",
+            "command": "SRANDMEMBER",
+            "args": [b"k", b"3"],
+            "returns": lambda rv, cs: cs.data[b'k'].issuperset(set(rv)),
+            "depends": [
+                {
+                    "command": "SADD",
+                    "args": [b"k", b"1", b"2", b"3", b"4", b"5"],
+                    "returns": 1,
+                }
+            ],
+        },
+        {
+            "name": "Check SRANDMEMBER with count bigger than set size",
+            "command": "SRANDMEMBER",
+            "args": [b"k", b"3"],
+            "returns": list(set([b"1", b"2"])),
+            "depends": [{"command": "SADD", "args": [b"k", b"1", b"2"], "returns": 2}],
+        },
+
     ]
 
     for item in test_sequence:
@@ -659,8 +963,15 @@ def test_commands():
                 cmd = getattr(cs, d_item["command"])
                 cmd(*d_item["args"])
         # real test here
-        returns = getattr(cs, item["command"])(*item["args"])
-        assert returns == protocolBuilder(item["returns"]), item["name"]
+        if "returns" in item:
+            retval = getattr(cs, item["command"])(*item["args"])
+
+            returns = item["returns"]
+            if callable(returns):
+                print (protocolParser(retval), cs.data, returns)
+                assert returns(protocolParser(retval), cs)  # should return truthy
+            else:
+                assert retval == protocolBuilder(returns), item["name"]
 
         if "expects" in item:
             assert item["expects"](cs), f"expects of {item['name']}"
